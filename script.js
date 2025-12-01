52c67ffec4ad132c82beddee5285384f4845effe
Anass Mokhless <anass.mokhless123@gmail.com>
2025-12-01 12:43:40 +0100

    first steps in js

--- script.js
+++ script.js
@@ -1,1 +1,25 @@
-console.log("ready");
+const board = ["pink","blue","green","red","purple"];
+const myBoard = [];
+const ghosts = [];
+const g = {
+    x:"",
+    y:"",
+    h:100,
+    size:25,
+    ghosts:6,
+    inplay:false
+}
+const player ={
+    pos:20,
+    speed:4,
+    cool:0,
+    pause:false
+}
+
+document.addEventListener("DOMContentLoaded", () => {
+    g.grid = document.querySelector(".grid"); //gameBoard
+    g.pacman = document.querySelector(".pacman"); //pacman
+
+    console.log(g);
+    console.log(pacman);
+})
